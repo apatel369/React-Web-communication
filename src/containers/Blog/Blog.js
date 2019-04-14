5c080425e9d0236f6e15ad5ea067d81c6aa760a9
Arpan Patel <ap.patel1893@gmail.com>
2019-04-13 23:11:12 -0500

    apply routing

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+// import axios from 'axios';
+import axios from '../../axios';
+import { Route, Link } from 'react-router-dom';
+
+import './Blog.css';
+import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost'
+
+class Blog extends Component {
+
+    render () {
+        return (
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to={{
+                                pathname: '/new-post',
+                                hash: '#submit',
+                                search: '?quick-submit=true'
+                            }}>New Post</Link></li>
+                        </ul>
+                    </nav>
+                </header>
+                {/* <Route path="/" exact render={() => <Posts></Posts>}/> */}
+                <Route path="/" exact component={Posts}/>
+                <Route path="/new-post" component={NewPost}/>
+            </div>
+        );
+    }
+}
+
+export default Blog;
